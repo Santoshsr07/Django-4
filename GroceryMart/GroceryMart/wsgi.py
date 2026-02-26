9b216b0f6007b2e7bfc3cfae41ca8baf836feb2c
Santoshsr07 <santoshsr7096@gmail.com>
2026-02-26 21:49:38 +0530

    reorder whole structure

--- GroceryMart/GroceryMart/wsgi.py
+++ GroceryMart/GroceryMart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flipkart project.
+WSGI config for GroceryMart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipkart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GroceryMart.settings')
 
 application = get_wsgi_application()
